5c51ec3760672af6e6ebaff4d72cbc82dfdf1f57
ChuanLiu0316 <liuchuan95@gmail.com>
2016-03-29 20:09:28 -0400

    bug

--- Passges.py
+++ Passges.py
@@ -1,10 +1,9 @@
 from Passage import *
 
 class Passages:
-    
-    self.passages = []
-
+    ASDASD = 0
     def __init__(self, file):
+        self.passages = []
         with open(file) as tsv:
             for line in csv.reader(tsv, delimiter='\t'):
                 newPassage =  Passage(line)
