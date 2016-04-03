f5e67b42b38e768831c74f6abc64326f63cccb39
ChuanLiu0316 <liuchuan95@gmail.com>
2016-04-03 17:53:06 -0400

    finish distance-based algo, need final test for correct rate

---
+++ DisBased.py
@@ -0,0 +1,33 @@
+#Distance Based Algorithm run on one passage
+from Passage import * 
+class DistanceBased:
+    # the algorithm doesn't return one answer but scores for 4 answers, easy to optimize later
+    @classmethod
+    def run(self, passage):
+        def score(SQ, SA):
+            Qindexes = [i for i,x in enumerate(passage.passage) if x in SQ]
+            Aindexes = [i for i,x in enumerate(passage.passage) if x in SA]
+            min = len(passage.passage)
+            for Aindex in Aindexes:
+                for Qindex in Qindexes:
+                    if abs(Qindex - Aindex) < min:
+                        min = abs(Qindex - Aindex)
+            return float(min)/len(passage.passage)            
+        
+        answersScores = []
+        for i in range(4):
+            Q =  passage.questions[i]    
+            Qset = Q.questionWords  
+            
+            SQ = Qset.intersection(passage.passageWords)
+            ScoreArray = []
+            for j in range(4):
+                SAI = (Q.answersWords[j].intersection(passage.passageWords)).difference(Qset)
+                if len(SQ) == 0 or len(SAI) == 0:
+                    ScoreArray.append(1.0)
+                else:
+                    ScoreArray.append(score(SQ, SAI))
+
+            answersScores.append(ScoreArray)
+
+        return answersScores  
--- SWandDAlgo.py
+++ SWandDAlgo.py
@@ -1,1 +1,16 @@
-# Sliding Window + Distance Based Algorithm 
+#Distance Based Algorithm run on one passage
+from Passage import * 
+from DisBased import *
+from SWAlgo import *
+
+class SWDalgo:
+    # the algorithm doesn't return one answer but scores for 4 answers, easy to optimize later
+    @classmethod
+    def run(self, passage):     
+        Dscores = DistanceBased.run(passage)
+        SWscores = SlidingWindow.run(passage)
+        for i in range(len(SWscores)):
+            for j in range(len(SWscores[i])):
+                SWscores[i][j] = SWscores[i][j] - Dscores[i][j]
+
+        return  SWscores
